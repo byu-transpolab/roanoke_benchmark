8f23a084249de8d70cea59d7aae0b65e41da5be7
Greg Macfarlane <gregmacfarlane@gmail.com>
2024-11-13 15:20:15 -0700

    Do a little bit of cleanup on the code, kayleigh will do more

--- network.py
+++ network.py
@@ -3,51 +3,41 @@ from simpledbf import Dbf5
 import geopandas as gpd
 
 # read the link dbf to a csv and export
-def dbflinks_to_csv(dbf_file, csv_file):
+def dbflinks_to_csv(dbf_file,  shapefile, csv_file):
     # Read the DBF file
     dbf = Dbf5(dbf_file)
     df = dbf.to_dataframe()
+    
+    # edit the dbf file from cube to have the fields we want
+    # Define the columns you want to keep
+    new_order = ['ID', 'RTE_NAME', 'A', 'B', 'DIR', 'DISTANCE', 'FACTYPE', 'CAP_R', 'FFSPEED_R', 'LANES']  # Replace with your desired columns
+    
+    # Drop columns that are not in the new order
+    df = df[new_order]
+    df.rename(columns={'ID': 'link_id', 'RTE_NAME': 'name', 'A': 'from_node_id', 'B': 'to_node_id', 'DIR': 'directed', 'DISTANCE': 'length', 'FACTYPE': 'facility_type', 'CAP_R': 'capacity', 'FFSPEED_R': 'free_speed', 'LANES': 'lanes'}, inplace=True)
 
-    # Load the existing CSV file
-    csv_path = 'links.csv'
-    existing_df = pd.read_csv(csv_path)
-
-    # Load the shapefile
-    shapefile_path = 'links shape.shp'
-    gdf = gpd.read_file(shapefile_path)
+    # Load the shapefile to append geometry
+    gdf = gpd.read_file(shapefile)
 
     # Select the column you want to export (replace 'your_column' with the actual column name)
     new_column = gdf[['geometry']]
 
     # Concatenate the new column to the existing DataFrame
-    combined_df = pd.concat([existing_df, new_column], axis=1)
+    # TODO: fix so that we join by ID instead of by place.
+    combined_df = pd.concat([df, new_column], axis=1)
 
     # Save the combined DataFrame back to the CSV file
-    combined_df.to_csv(csv_path, index=False)
-    #process link information
-    #process link information
-
-    # Define the columns you want to keep
-    new_order = ['ID', 'RTE_NAME', 'A', 'B', 'geometry', 'DIR', 'DISTANCE', 'FACTYPE', 'CAP_R', 'FFSPEED_R', 'LANES']  # Replace with your desired columns
-    
-    # Drop columns that are not in the new order
-    df = df[new_order]
-    df.rename(columns={'ID': 'link_id', 'RTE_NAME': 'name', 'A': 'from_node_id', 'B': 'to_node_id', 'DIR': 'directed', 'DISTANCE': 'length', 'FACTYPE': 'facility_type', 'CAP_R': 'capacity', 'FFSPEED_R': 'free_speed', 'LANES': 'lanes'}, inplace=True)
-    # Save the modified DataFrame back to the same CSV file
-    # Write to CSV file
-    df.to_csv(csv_file, index=False)
+    combined_df.to_csv(csv_file, index=False)
 
 
-def dbfnodes_to_csv(dbf_file, csv_file):
-    # Read the DBF file
-    dbf = Dbf5(dbf_file)
-    df = dbf.to_dataframe()
+def dbfnodes_to_csv(in_csv_file, zones_file, out_csv_file):
 
     # Load the source and destination CSV files
     source_file = 'zones.csv'
     destination_file = 'nodes.csv'
 
     # Read the CSV files into DataFrames
+    # TODO: Kayleigh to clean up and make it a linear process.
     source_df = pd.read_csv(source_file)
     destination_df = pd.read_csv(destination_file)
 
@@ -77,7 +67,7 @@ def dbfnodes_to_csv(dbf_file, csv_file):
     df.rename(columns={'N': 'node_id', 'X': 'x_coord', 'Y': 'y_coord', 'Z': 'zone_id'}, inplace=True)
 
     # Step 3: Save the modified DataFrame back to a CSV file
-    df.to_csv('nodes.csv', index=False)
+    df.to_csv(out_csv_file, index=False)
 
 
 
@@ -94,7 +84,7 @@ if __name__ == "__main__":
 
 
     # read in nodes and write out CSV file
-    inputn_dbf = 'nodes.dbf'    # Replace with your DBF file path
+    inputn_csv = 'nodes_from_cube.csv'    # Replace with your DBF file path
     outputn_csv = 'nodes.csv'   # Replace with your desired CSV file path
 
     dbfnodes_to_csv(inputn_dbf, outputn_csv)
