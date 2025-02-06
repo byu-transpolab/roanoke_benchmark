3590e03299fd4cd4742ecde579ad88ddd24935a5
Kaleigh Squires <kmsquire@byu.edu>
2025-02-06 10:29:42 -0700

    fixed zone ID column

--- py/network.py
+++ py/network.py
@@ -166,7 +166,6 @@ def dbfoutputs_to_csv(dbf_file, csv_file):
 def dbfnodes_to_csv(dbf_file, csv_file):
     df = read_dbf(dbf_file)
     # Rename columns for clarity
-    df.rename(columns={'N': 'ID'}, inplace=True)
    
     # Save to CSV
     save_to_csv(df, csv_file)
@@ -179,12 +178,12 @@ def merge_zones_with_nodes(nodes_csv, zones_csv, output_csv):
     zones_df = pd.read_csv(zones_csv)
    
     # Merge based on 'ID' column and save the result
-    merged_df = pd.merge(nodes_df, zones_df[['ID', 'Z']], on='ID', how='left')
+    merged_df = pd.merge(nodes_df, zones_df[['N', 'Z']], on='N', how='left')
 
     # Add the `is_centroid` column
     merged_df['is_centroid'] = merged_df['Z'].apply(lambda z: 1 if pd.notna(z) and float(z).is_integer() else 0)
 
-    merged_df.rename(columns={'Z': 'zone_id', 'ID': 'node_id', 'X': 'x_coord', 'Y': 'y_coord'}, inplace=True)
+    merged_df.rename(columns={'Z': 'zone_id', 'N': 'node_id', 'X': 'x_coord', 'Y': 'y_coord'}, inplace=True)
 
     # Save the result to CSV
     save_to_csv(merged_df, output_csv)
